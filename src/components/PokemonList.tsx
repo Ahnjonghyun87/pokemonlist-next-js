9e5ff8aac1d619e902bd68b23bab8b25b7cae8b0
[Github <helgo1318@gmail.com>
2024-07-05 12:38:53 +0900

    test2

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -6,7 +6,7 @@ import { AxiosError } from "axios";
 import Image from "next/image";
 import Link from "next/link";
 
-const PokemonListPage = (): JSX.Element => {
+const PokemonListPage = () => {
   const {
     data: pokemons,
     isPending,
@@ -18,6 +18,7 @@ const PokemonListPage = (): JSX.Element => {
       const response = await fetch("/api/pokemon");
       return response.json();
     },
+    staleTime: 5 * 60 * 1000, // 5분
   });
 
   if (isPending) {
@@ -39,16 +40,13 @@ const PokemonListPage = (): JSX.Element => {
     return <div>무언가 잘못되었습니다{error.message}</div>;
   }
 
-  // const [pokemons, setPokemons] = React.useState<Pokemon[]>([]);
-
-  // React.useEffect(() => {
-  //   const fetchInitialData = async () => {
-  //     const response = await fetch("/api/pokemon");
-  //     const data = await response.json();
-  //     setPokemons(data);
-  //   };
-  //   fetchInitialData();
-  // }, []);
+  if (!Array.isArray(pokemons)) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-xl font-semibold">데이터가 올바르지 않습니다.</p>
+      </div>
+    );
+  }
 
   return (
     <div className="container mx-auto">
@@ -76,38 +74,3 @@ const PokemonListPage = (): JSX.Element => {
 };
 
 export default PokemonListPage;
-
-// <div className="container mx-auto">
-//       {pokemons.length === 0 ? (
-//         <div className="flex flex-col justify-center items-center">
-//           <Image
-//             src="/images/monsterball.png"
-//             alt="Loading..."
-//             width={256}
-//             height={256}
-//             className="mb-4 animate-bounce"
-//           />
-//           <p className="text-xl font-semibold">불러오는 중..</p>
-//         </div>
-//       ) : (
-//         <div className="grid grid-cols-2 sm:grid-cols3 md:grid-cols-4 lg:grid-cols-6 gap-4 item-center">
-//           {pokemons.map((pokemon) => (
-//             <div
-//               key={pokemon.id}
-//               className="pokemon p-4 border rounded-lg flex flex-col items-center"
-//             >
-//               <Link href={`/pokemon/${pokemon.id}`}>
-//                 <Image
-//                   src={pokemon.sprites.front_default}
-//                   alt={pokemon.korean_name}
-//                   width={96}
-//                   height={96}
-//                 />
-//                 <p>{pokemon.korean_name}</p>
-//                 <p>도감번호: {pokemon.id}</p>
-//               </Link>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </div>
