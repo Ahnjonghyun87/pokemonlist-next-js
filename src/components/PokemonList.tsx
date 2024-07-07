1a51e0d8d774e7df7198d197455d5ef6b05d2632
[Github <helgo1318@gmail.com>
2024-07-07 14:51:20 +0900

    리팩토링실패.함수오버로드오류남.

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import Pagination from "@/components/Pagination";
-import { Pokemon } from "@/types";
-import { useQuery } from "@tanstack/react-query";
-import { AxiosError } from "axios";
+import { Pokemon } from '@/types';
+import { useInfiniteQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
@@ -13,21 +11,28 @@ const ITEMS_PER_PAGE = 30;
 const PokemonListPage = () => {
   const [page, setPage] = useState(1);
 
-  const { data, isPending, error } = useQuery<
-    {
-      pokemons: Pokemon[];
-      totalCount: number;
-    },
-    AxiosError
-  >({
-    queryKey: ["pokemons", page],
-    queryFn: async () => {
+  const { data, isPending, error, fetchNextPage, hasNextPage } = useInfiniteQuery<{pokemons: Pokemon[]}>
+  ({
+    queryKey: ["Pokemons"],
+    initialPageParam:1,
+    queryFn: async ({pageParam}) => {
       const response = await fetch(
-        `/api/pokemon?_page=${page}&_limit=${ITEMS_PER_PAGE}`
-      );
-      const result = await response.json();
-      return { pokemons: result.pokemons, totalCount: result.totalCount };
+        "/api/pokemon",{params:{ _page:pageParam, _limit=ITEMS_PER_PAGE},
+});
+
+      return {
+        pokemons:response.json(),
+        totalPages.Math.ceil(
+          response.headers["x-total-count"] / ITEMS_PER_PAGE,
+        ),
+      };
     },
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      const nextPage = lastPageParam + 1;
+      return nextPage <= lastPage.totalPages ? nextPage : undefined;
+    },
+    select:({pages}) =>
+     pages.map((pokemonsPerPage) => pokemonsPerPage.pokemons)flat(),
     staleTime: 5 * 60 * 1000, // 5분
   });
 
@@ -82,95 +87,13 @@ const PokemonListPage = () => {
           </div>
         ))}
       </div>
-      <Pagination totalPages={totalPages} page={page} setPage={setPage} />
+      {hasNextPage && (
+  <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+    {isFetchingNextPage ? "로딩중..." : "더보기"}
+  </button>
+)}
     </div>
   );
 };
 
 export default PokemonListPage;
-
-// "use client";
-
-// import { Pokemon } from "@/types";
-// import { useQuery } from "@tanstack/react-query";
-// import { AxiosError } from "axios";
-// import Image from "next/image";
-// import Link from "next/link";
-
-// const ITEM_PER_PAGE = 30;
-
-// const PokemonListPage = () => {
-//   const [page, setPage] = useState(1);
-
-//   const {
-//     data: pokemons,
-//     isPending,
-//     error,
-//   } = useQuery<Pokemon[], AxiosError, Pokemon[], [string]>({
-//     queryKey: ["pokemons", page],
-
-//     queryFn: async () => {
-//       const response = await fetch("/api/pokemon", {
-//         params:{_page:page, _limit:ITEM_PER_PAGE},
-//       });
-//       return {
-//         pokemons:response.json(),
-//         totalCount: response.headers["x-total-count"],
-//       }
-//     },
-//     staleTime: 5 * 60 * 1000, // 5분
-//   });
-
-//   if (isPending) {
-//     return (
-//       <div className="flex flex-col justify-center items-center">
-//         <Image
-//           src="/images/monsterball.png"
-//           alt="Loading..."
-//           width={256}
-//           height={256}
-//           className="mb-4 animate-bounce"
-//         />
-//         <p className="text-xl font-semibold">불러오는 중..</p>
-//       </div>
-//     );
-//   }
-
-//   if (error) {
-//     return <div>무언가 잘못되었습니다{error.message}</div>;
-//   }
-
-//   if (!Array.isArray(pokemons)) {
-//     return (
-//       <div className="flex flex-col justify-center items-center">
-//         <p className="text-xl font-semibold">데이터가 올바르지 않습니다.</p>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="container mx-auto">
-//       <div className="grid grid-cols-2 sm:grid-cols3 md:grid-cols-4 lg:grid-cols-6 gap-4 item-center">
-//         {pokemons.map((pokemon) => (
-//           <div
-//             key={pokemon.id}
-//             className="pokemon p-4 border rounded-lg flex flex-col items-center"
-//           >
-//             <Link href={`/pokemon/${pokemon.id}`}>
-//               <Image
-//                 src={pokemon.sprites.front_default}
-//                 alt={pokemon.korean_name}
-//                 width={96}
-//                 height={96}
-//               />
-//               <p>{pokemon.korean_name}</p>
-//               <p>도감번호: {pokemon.id}</p>
-//             </Link>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PokemonListPage;
